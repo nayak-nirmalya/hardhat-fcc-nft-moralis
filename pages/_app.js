d2f576292c666500caf65b0ab28dfada0e2da914
Nirmalya Nayak <n.nayak263@gmail.com>
2022-07-30 11:32:13 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { MoralisProvider } from 'react-moralis'
+import Header from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      <Header />
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
