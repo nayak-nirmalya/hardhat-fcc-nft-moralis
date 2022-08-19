29385478dbfcce197cb6bfc6fcebf2754f6f4e44
Nirmalya Nayak <n.nayak263@gmail.com>
2022-08-19 16:06:55 +0530

    show NFT Attributes in index.js page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
+import { useMoralisQuery } from 'react-moralis'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <div className={styles.container}>Hi, Index Page!</div>
+  const {
+    data: listedNfts,
+    isFetching: fetchingListedNfts,
+  } = useMoralisQuery('ActiveItem', (query) =>
+    query.limit(10).descending('tokenId'),
+  )
+  console.log(listedNfts)
+
+  return (
+    <div className={styles.container}>
+      {fetchingListedNfts ? (
+        <div>Loading...</div>
+      ) : (
+        listedNfts.map((nft) => {
+          console.log(nft.attributes)
+          const {
+            price,
+            nftAddress,
+            tokenId,
+            marketplaceAddress,
+            seller,
+          } = nft.attributes
+          return (
+            <div>
+              Price: {price}. NFTAddress: {nftAddress}. TokenID: {tokenId}.
+              Seller: {seller}
+            </div>
+          )
+        })
+      )}
+    </div>
+  )
 }
