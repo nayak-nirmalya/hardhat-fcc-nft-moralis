3cd973ef606e743bb63048858693b33a4c03907f
Nirmalya Nayak <n.nayak263@gmail.com>
2022-08-19 17:20:55 +0530

    added NFTBox component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import { useMoralisQuery } from 'react-moralis'
+import { useMoralisQuery, useMoralis } from 'react-moralis'
+import NFTBox from '../components/NFTBox'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const { isWeb3Enabled } = useMoralis()
   const {
     data: listedNfts,
     isFetching: fetchingListedNfts,
@@ -11,27 +13,40 @@ export default function Home() {
   console.log(listedNfts)
 
   return (
-    <div className={styles.container}>
-      {fetchingListedNfts ? (
-        <div>Loading...</div>
-      ) : (
-        listedNfts.map((nft) => {
-          console.log(nft.attributes)
-          const {
-            price,
-            nftAddress,
-            tokenId,
-            marketplaceAddress,
-            seller,
-          } = nft.attributes
-          return (
-            <div>
-              Price: {price}. NFTAddress: {nftAddress}. TokenID: {tokenId}.
-              Seller: {seller}
-            </div>
+    <div className="container mx-auto">
+      <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+      <div className="flex flex-wrap">
+        {isWeb3Enabled ? (
+          fetchingListedNfts ? (
+            <div>Loading...</div>
+          ) : (
+            listedNfts.map((nft) => {
+              console.log(nft.attributes)
+              const {
+                price,
+                nftAddress,
+                tokenId,
+                marketplaceAddress,
+                seller,
+              } = nft.attributes
+              return (
+                <div>
+                  <NFTBox
+                    price={price}
+                    nftAddress={nftAddress}
+                    tokenId={tokenId}
+                    marketplaceAddress={marketplaceAddress}
+                    seller={seller}
+                    key={`${nftAddress}${tokenId}`}
+                  />
+                </div>
+              )
+            })
           )
-        })
-      )}
+        ) : (
+          <div>Web3 Currently Not Enabled!</div>
+        )}
+      </div>
     </div>
   )
 }
